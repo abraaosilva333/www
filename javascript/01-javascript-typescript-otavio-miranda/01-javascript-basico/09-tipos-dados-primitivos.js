9de1c4d477a8fd8d5563181a6885c2340db31836
Abraão Silva <abraaosilva@email.com>
2026-08-21 19:18:27 -0300

    feat: adiciona aula sobre "tipos de dados primitivos"

---
+++ javascript/01-javascript-typescript-otavio-miranda/01-javascript-basico/09-tipos-dados-primitivos.js
@@ -0,0 +1,21 @@
+// string, number, undefined, null, boolean, symbol;
+
+// string
+let name = "Abraão";
+
+// number
+let num = 10;
+
+// boolean
+// true, false
+let bol = false;
+
+// existe algo, mas nenhum valor foi definido;
+let name;
+
+console.log(name); // undefined
+
+// você definiu intencionalmente que não existe um valor;
+let lastName = null;
+
+console.log(lastName); // null
