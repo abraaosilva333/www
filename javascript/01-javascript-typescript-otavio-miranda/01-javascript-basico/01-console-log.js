36769ecf6fe4214e17011dab7e08ab0513d6e220
Abraão Silva <abraaosilva@email.com>
2026-08-21 14:15:33 -0300

    feat: adiciona aula sobre "console.log"

---
+++ javascript/01-javascript-typescript-otavio-miranda/01-javascript-basico/01-console-log.js
@@ -0,0 +1,13 @@
+// String
+console.log(typeof("Olá, mundo!"));
+console.log('Olá, mundo!');
+console.log("'Olá, 'mundo!");
+console.log('Olá, "mundo!"');
+console.log(`"Olá, "'mundo!'`);
+
+// Numbers
+console.log(typeof(123));
+console.log(123.456);
+
+// String + Number
+console.log(123, 123.456, "Abraão");
