72d2ea0efaafacb54c639891fa11906a9fb84fc0
Abraão Silva <abraaosilva@email.com>
2026-08-21 16:53:53 -0300

    feat: adiciona aula sobre "variáveis com let"

---
+++ javascript/01-javascript-typescript-otavio-miranda/01-javascript-basico/05-variaveis-let.js
@@ -0,0 +1,15 @@
+let joaoName = "João"; // String
+let joaoBirthYear = 1984; // Number
+let wifeName = "Maria"; // String
+let marriageYear = 2012; // Number
+let numberOfChildren = 1; // Number
+let childBirthYear = 2015; // Number
+let childName = "Eduardo"; // String
+
+console.log(`${joaoName} nasceu em ${joaoBirthYear}.`);
+console.log(`Em 2000 ${joaoName} conheceu ${wifeName}.`);
+console.log(`${joaoName} casou-se com ${wifeName} em ${marriageYear}.`);
+console.log(
+  `${wifeName} teve ${numberOfChildren} filho com ${joaoName} em ${childBirthYear}.`,
+);
+console.log(`O filho de ${joaoName} se chama ${childName}.`);
