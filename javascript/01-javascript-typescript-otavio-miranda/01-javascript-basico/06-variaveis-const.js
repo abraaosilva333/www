5c62b1d9e35e7bc02b893fa966dda800f695d63e
Abraão Silva <abraaosilva@email.com>
2026-08-21 17:29:15 -0300

    feat: adiciona aula sobre "variáveis com const"

---
+++ javascript/01-javascript-typescript-otavio-miranda/01-javascript-basico/06-variaveis-const.js
@@ -0,0 +1,32 @@
+/* const joaoName = "João"; // String
+const joaoBirthYear = 1984; // Number
+const wifeName = "Maria"; // String
+const marriageYear = 2012; // Number
+let numberOfChildren = 1; // Number
+const childBirthYear = 2015; // Number
+const childName = "Eduardo"; // String */
+
+/* console.log(`${joaoName} nasceu em ${joaoBirthYear}.`);
+console.log(`Em 2000 ${joaoName} conheceu ${wifeName}.`);
+console.log(`${joaoName} casou-se com ${wifeName} em ${marriageYear}.`);
+console.log(
+  `${wifeName} teve ${numberOfChildren} filho com ${joaoName} em ${childBirthYear}.`,
+);
+console.log(`O filho de ${joaoName} se chama ${childName}.`); */
+
+// Operadores aritméticos básicos
+// +, -, *, /
+const firstNumber = 5;
+const secondNumber = 10;
+const result = firstNumber * secondNumber;
+const duplicateResult = result * 2;
+let tripleResult = result * 3;
+tripleResult = tripleResult + 5;
+
+// console.log(firstNumber + secondNumber);
+// console.log(result);
+// console.log(duplicateValue);
+console.log(tripleResult)
+
+// Retornar o tipo da variável
+console.log(`Tipo da variável (tripleResult): ${typeof (tripleResult)}`)
