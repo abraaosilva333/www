766a659e697d5f82f09805097c24289cf2ca714f
Abraão Silva <abraaosilva@email.com>
2026-08-22 00:37:58 -0300

    feat: adiciona exercício sobre "alert, confirm, prompt"

---
+++ javascript/01-javascript-typescript-otavio-miranda/01-javascript-basico/12-exercicio-alert-confirm-prompt.js
@@ -0,0 +1,6 @@
+const numberOne = Number(prompt("Informe um número"));
+const numberTwo = Number(prompt("Informe um número"));
+
+const result = numberOne + numberTwo;
+
+alert(`Resultado: ${result}`);
