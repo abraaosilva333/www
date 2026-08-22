79e0b7ce2a257b8d9d0c4f59d7e628d8e6b74d7f
Abraão Silva <abraaosilva@email.com>
2026-08-22 00:24:06 -0300

    feat: adiciona aula sobre "alert, confirm, prompt"

---
+++ javascript/01-javascript-typescript-otavio-miranda/01-javascript-basico/11-alert-confirm-prompt.js
@@ -0,0 +1,7 @@
+const alerta = window.alert("alert");
+const confirma = window.confirm("confirm");
+const promp = window.prompt("prompt");
+
+console.log(alerta);
+console.log(confirma);
+console.log(promp);
