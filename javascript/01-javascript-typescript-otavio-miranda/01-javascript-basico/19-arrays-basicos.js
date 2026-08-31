41dbddd81aeaa3df61f0d8b629d2c8474ff35b9c
Abraão Silva <abraaosilva@email.com>
2026-08-30 21:50:54 -0300

    feat: adiciona exercícios básicos sobre números e arrays em JavaScript

---
+++ javascript/01-javascript-typescript-otavio-miranda/01-javascript-basico/19-arrays-basicos.js
@@ -0,0 +1,31 @@
+const alunos = ["Maria", "Ana", "Julia"];
+
+console.log(alunos);
+console.log(alunos.slice(0, 2));
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+
+// alunos[alunos.length] = "Francisca";
+// alunos[alunos.length] = "Antonia";
+// alunos[alunos.length] = "Adriana";
+
+/* alunos.push("Francisca", "Antonia", "Adriana"); // push() -> coloca os dados no final do array
+alunos.unshift("Jesus", "José"); // unshift() -> coloca os dados no final do array */
+/* alunos.pop(); // pop() -> remover dado do final do array
+delete alunos[2]; // remover o dado mas mentendo o índice dele
+alunos.shift(); // shift() -> remove dado do começo do array */
+
+// const removidosFinal = alunos.pop(); 
+// const removidosComeco = alunos.shift();
+
+// console.table(removidosFinal);
+// console.table(removidosComeco);
+
+// alunos[2] = "Francisca";
+
+// console.log(alunos);
+// console.table(alunos);
+// console.log(alunos[2]);
+// console.log(alunos);
+// console.log(alunos.length);
